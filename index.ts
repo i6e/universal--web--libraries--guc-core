1832473ad24c98c8b6b5575805527016235556af
Juyeong Maing <mjy9088@naver.com>
2024-08-30 03:15:09 +0900

    expose missing api

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 export { cssContent } from "./lib/cssContent";
+export { executionContext } from "./lib/executionContext";
 export { scanClasses } from "./lib/scanClasses";
 export { stringify } from "./lib/stringify";
